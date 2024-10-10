e0aaea2dd99a6277886207991723db419822c1cb
matiaspietrafesa1 <mp318802@fi365.ort.edu.uy>
2024-10-10 09:37:51 -0300

    Add files via upload

---
+++ LogicaNegocio/Subasta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaVentas
+{
+    internal class Subasta : Publicacion
+    {
+        private List<Oferta> _ofertas;
+
+        public Subasta(string nombre, string estado, List<Articulo> articulos, List<Oferta> ofertas)
+            : base (nombre, estado, articulos)
+        {
+            _ofertas = ofertas;
+        }
+
+        public override string ToString()
+        {
+            //string texto = "";
+            //if (Estado.Trim().ToUpper() == "ABIERTA")
+            //{
+            //    texto = $"({Id}) - {Nombre} - Abierta\n{ListarArticulos}{ListarOfertas}";
+            //} else if (Estado.Trim().ToUpper() == "CERRADA")
+            //{
+            //    texto = $"({Id}) - {Nombre} - {Estado} - {FechaFinalizacion}\n{ListarArticulos}Ganador: {Cliente} - Cerrado por: {Usuario}{ListarOfertas}";
+            //}
+            //return texto;
+            return $"({Id}) - {Nombre} - {Estado} - {FechaPublicacion.ToShortDateString()}";
+        }
+
+        //private void ListarArticulos() 
+        //{
+        //    Console.WriteLine("Articulos:");
+        //    foreach(Articulo a in Articulos)
+        //    {
+        //        Console.WriteLine("\t" + a.ToString());
+        //    }
+        //}
+
+        //private void ListarOfertas()
+        //{
+        //    Console.WriteLine("Ofertas:");
+        //    foreach (Oferta o in _ofertas)
+        //    {
+        //        Console.WriteLine("\t" + o.ToString());
+        //    }
+        //}
+    }
+}
---
+++ LogicaNegocio/Venta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaVentas
+{
+    public class Venta : Publicacion
+    {
+        private bool _ofertaRelampago;
+
+        public Venta(string nombre, string estado, List<Articulo> articulos , bool ofertaRelampago)
+            : base(nombre, estado, articulos)
+        {
+            _ofertaRelampago = ofertaRelampago;
+        }
+
+        public override string ToString()
+        {
+            return $"({Id}) - {Nombre} - {Estado} - {FechaPublicacion.ToShortDateString()}";
+        }
+    }
+}
